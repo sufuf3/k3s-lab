b3d5dc83e14729bdd472a49bf868858eaf0db5d5
Samina Fu <sufuf3@gmail.com>
2019-09-23 23:44:07 +0800

    Update install script from containerd to docker

--- install-k3s-node.sh
+++ install-k3s-node.sh
@@ -6,9 +6,18 @@ NODE=$(hostname)
 sudo apt update
 sudo apt install jq vim -y
 #echo "$K3SMASTERIPADDRESS       $K3SMASTER" | sudo tee -a /etc/hosts
+# Install Docker
+# kubernetes official max validated version: 1.13.1, 17.03, 17.06, 17.09, 18.06, 18.09 (Ref: https://kubernetes.io/docs/setup/release/notes/#unchanged)
+export DOCKER_VERSION="18.06.3~ce~3-0~ubuntu"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce=${DOCKER_VERSION}
+# Manage Docker as a non-root user
+sudo usermod -aG docker $USER
+
+# Install K3s-angent
 IPADDR=$(ip a show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f1)
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v0.9.0 INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-iface=enp0s8" K3S_URL=https://192.168.0.200:6443 K3S_TOKEN=$NODE_TOKEN sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v0.9.0 INSTALL_K3S_EXEC="--docker --node-ip=${IPADDR} --flannel-iface=enp0s8" K3S_URL=https://192.168.0.200:6443 K3S_TOKEN=$NODE_TOKEN sh -
 systemctl status k3s-agent --no-pager
 #journalctl -u k3s-agent
-sleep 10
-tail /var/lib/rancher/k3s/agent/containerd/containerd.log
--- install-k3s-server.sh
+++ install-k3s-server.sh
@@ -2,8 +2,19 @@
 MASTER=$(hostname)
 sudo apt update
 sudo apt install jq vim git -y
+# Install Docker
+# kubernetes official max validated version: 1.13.1, 17.03, 17.06, 17.09, 18.06, 18.09 (Ref: https://kubernetes.io/docs/setup/release/notes/#unchanged)
+export DOCKER_VERSION="18.06.3~ce~3-0~ubuntu"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce=${DOCKER_VERSION}
+# Manage Docker as a non-root user
+sudo usermod -aG docker $USER
+
+# Install K3s
 IPADDR=$(ip a show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f1)
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-iface=enp0s8 --write-kubeconfig-mode 644 --no-deploy=servicelb --no-deploy=traefik" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--docker --node-ip=${IPADDR} --flannel-iface=enp0s8 --write-kubeconfig-mode 644 --no-deploy=servicelb --no-deploy=traefik" sh -
 systemctl status k3s --no-pager
 #kubectl taint nodes $MASTER node-role.kubernetes.io/master=true:NoSchedule
 #kubectl label node $MASTER kubernetes.io/role=master node-role.kubernetes.io/master=
